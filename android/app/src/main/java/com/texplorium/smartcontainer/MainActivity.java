a3ae0ba3167c3dcaff7b30cdaa5ac28c8d5a5fea
kailashnath1998 <kailashnath1998@gmail.com>
2019-02-02 19:46:28 +0530

    added android init
    Signed-off-by: kailashnath1998 <kailashnath1998@gmail.com>

---
+++ android/app/src/main/java/com/texplorium/smartcontainer/MainActivity.java
@@ -0,0 +1,25 @@
+package com.texplorium.smartcontainer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.jjoe64.graphview.*;
+import com.jjoe64.graphview.series.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+    }
+}
